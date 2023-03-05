2131df6a9434a366bb61ce4b02391032b06ca9c5
Juan Lopez <camilo.lopez@foonkiemonkey.co.uk>
2023-03-05 16:58:37 -0500

    Initial Commit

---
+++ CatsApp/CatsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatsAppApp.swift
+//  CatsApp
+//
+//  Created by Cristian on 5/03/23.
+//
+
+import SwiftUI
+
+@main
+struct CatsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
